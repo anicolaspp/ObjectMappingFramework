afee2a908f6b94996e951ccb07037a6b5c957564
Nicolas A Perez <anicolaspp@gmail.com>
2013-08-24 23:49:55 -0400

    adding mapping without using the attribute. This is util when trying to map two object that weren't coded by user, when mapping two objects located on 3-party libraries

---
+++ src/Helpers/MapByName.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Reflection;
+
+namespace MappingObjectsFramework
+{
+	public class MapByName 
+	{
+		public string l;
+	}
+}
--- src/Helpers/MappingHelper.cs
+++ src/Helpers/MappingHelper.cs
@@ -5,133 +5,227 @@ using System.Reflection;
 
 namespace MappingObjectsFramework
 {
-    public static class MappingHelper
-    {
-        public static T MapTo<T>(this object obj, IEnumerable<Converter> converters) where T : new()
-        {
-            ConverterResolver converterResolver = new StandartConverterResolver(true);
+	public static class MappingHelper
+	{
+		private static T Map<T>(object obj, ConverterResolver converterResolver, bool useAttribute) where T : new()
+		{
+			if (converterResolver == null)
+			{
+				throw new ArgumentNullException("converterResolver");
+			}
 
-            foreach (var converter in converters)
-            {
-                converterResolver.Add(converter);
-            }
+			T result = new T();
 
-            return MapTo<T>(obj, converterResolver);
-        }
+			foreach (var propertyInfo in result.GetType().GetProperties())
+			{
+				PropertyInfo sourceProperty = null;
+			
+				if (useAttribute)
+				{
+					MapPropertyNameAttribute t = (MapPropertyNameAttribute)Attribute.GetCustomAttribute(propertyInfo, typeof(MapPropertyNameAttribute));
+	
+					if (t == null)
+						continue;
+	
+					sourceProperty = t.Type == null ? obj.GetType().GetProperty(t.Name) : obj.GetType().GetProperty(t.Name, t.Type);
+						
+					if (sourceProperty == null)
+					{
+						throw new InvalidMapException(string.Format("There is not a property on {0} object which matches with {1} from the target object", obj, t.Name));
+					}
+				}
+				else
+				{
+					sourceProperty = obj.GetType().GetProperty(propertyInfo.Name);
+					
+					if (sourceProperty == null)
+					{
+						continue;
+					}
+				}
+			
+				try
+				{ 
+					Type inType = sourceProperty.PropertyType; 
+					Type outType = propertyInfo.PropertyType;
+	
+					object valueToConvert = sourceProperty.GetValue(obj, null);
+	
+					if (valueToConvert != null)
+					{
+						MethodInfo genericMethod = converterResolver.GetType().GetMethod("Convert").MakeGenericMethod(inType, outType);
+						object valueToSet = genericMethod.Invoke(converterResolver, new[] { valueToConvert });
+	
+						propertyInfo.SetValue(result, valueToSet, null);
+					}
+					else
+					{
+						Expression<Func<object>> e = Expression.Lambda<Func<object>>(Expression.Convert(Expression.Default(outType), outType));
+	
+						object defaultValue = e.Compile()(); //outType.IsValueType ? Activator.CreateInstance(outType) : null;
+						propertyInfo.SetValue(result, defaultValue, null);
+					}
+				}
+				catch (Exception e)
+				{
+					throw new InvalidMapException(string.Format("The types of the properties {0} on {1} and {2} on {3} do not match. " +
+						"See Inner Exception for more details.", propertyInfo.Name,
+					                                            result.GetType().Name, sourceProperty.Name, obj.GetType().Name), e);
+				}
+			}
+				
+			
 
-        public static T MapTo<T>(this object obj, ConverterResolver converterResolver) where T : new()
-        {
-            if (converterResolver == null)
-            {
-                throw new ArgumentNullException("converterResolver");
-            }
+			return result;
+		}
 
-            T result = new T();
+		public static T MapTo<T>(this object obj, IEnumerable<Converter> converters) where T : new()
+		{
+			ConverterResolver converterResolver = new StandartConverterResolver(true);
 
-            foreach (var propertyInfo in result.GetType().GetProperties())
-            {
-                MapPropertyNameAttribute t = (MapPropertyNameAttribute)Attribute.GetCustomAttribute(propertyInfo, typeof(MapPropertyNameAttribute));
+			foreach (var converter in converters)
+			{
+				converterResolver.Add(converter);
+			}
 
-                if (t != null)
-                {
-                    PropertyInfo sourceProperty = t.Type == null ? obj.GetType().GetProperty(t.Name) : obj.GetType().GetProperty(t.Name, t.Type);
+			return MapTo<T>(obj, converterResolver);
+		}
 
-                    if (sourceProperty == null)
-                    {
-                        throw new InvalidMapException(string.Format("There is not a property on {0} object which matches with {1} from the target object", obj, t.Name));
-                    }
+		public static T MapTo<T>(this object obj, ConverterResolver converterResolver) where T : new()
+		{
+			return Map<T>(obj, converterResolver, true);
+        
+//            if (converterResolver == null)
+//            {
+//                throw new ArgumentNullException("converterResolver");
+//            }
+//
+//            T result = new T();
+//
+//            foreach (var propertyInfo in result.GetType().GetProperties())
+//            {
+//                MapPropertyNameAttribute t = (MapPropertyNameAttribute) Attribute.GetCustomAttribute(propertyInfo, typeof(MapPropertyNameAttribute));
+//
+//                if (t != null)
+//                {
+//                    PropertyInfo sourceProperty = t.Type == null ? obj.GetType().GetProperty(t.Name) : obj.GetType().GetProperty(t.Name, t.Type);
+//
+//                    if (sourceProperty == null)
+//                    {
+//                        throw new InvalidMapException(string.Format("There is not a property on {0} object which matches with {1} from the target object", obj, t.Name));
+//                    }
+//
+//                    try
+//                    {
+//                    	Type inType = sourceProperty.PropertyType;
+//                        Type outType = propertyInfo.PropertyType;
+//
+//                        object valueToConvert = sourceProperty.GetValue(obj, null);
+//
+//                        if (valueToConvert != null)
+//                        {
+//                            MethodInfo genericMethod = converterResolver.GetType().GetMethod("Convert").MakeGenericMethod(inType, outType);
+//                            object valueToSet = genericMethod.Invoke(converterResolver, new[] {valueToConvert});
+//
+//                            propertyInfo.SetValue(result, valueToSet, null);
+//                        }
+//                        else
+//                        {
+//                            Expression<Func<object>> e = Expression.Lambda<Func<object>>(Expression.Convert(Expression.Default(outType), outType));
+//
+//                            object defaultValue = e.Compile()(); //outType.IsValueType ? Activator.CreateInstance(outType) : null;
+//                            propertyInfo.SetValue(result, defaultValue, null);
+//                        }
+//
+//                    }
+//                    catch (Exception e)
+//                    {
+//                        throw new InvalidMapException(string.Format("The types of the properties {0} on {1} and {2} on {3} do not match. " +
+//                                                                    "See Inner Exception for more details.", propertyInfo.Name,
+//                            result.GetType().Name, sourceProperty.Name, obj.GetType().Name), e);
+//                    }
+//                }
+//            }
+//
+//            return result;
+		}
 
-                    try
-                    {
-                        Type outType = propertyInfo.PropertyType;
-                        Type inType = sourceProperty.PropertyType;
-
-                        object valueToConvert = sourceProperty.GetValue(obj, null);
-
-                        if (valueToConvert != null)
-                        {
-                            MethodInfo genericMethod = converterResolver.GetType().GetMethod("Convert").MakeGenericMethod(inType, outType);
-                            object valueToSet = genericMethod.Invoke(converterResolver, new[] {valueToConvert});
-
-                            propertyInfo.SetValue(result, valueToSet, null);
-                        }
-                        else
-                        {
-                            Expression<Func<object>> e = Expression.Lambda<Func<object>>(Expression.Convert(Expression.Default(outType), outType));
-
-                            object defaultValue = e.Compile()(); //outType.IsValueType ? Activator.CreateInstance(outType) : null;
-                            propertyInfo.SetValue(result, defaultValue, null);
-                        }
-
-                    }
-                    catch (Exception e)
-                    {
-                        throw new InvalidMapException(string.Format("The types of the properties {0} on {1} and {2} on {3} do not match. " +
-                                                                    "See Inner Exception for more details.", propertyInfo.Name,
-                            result.GetType().Name, sourceProperty.Name, obj.GetType().Name), e);
-                    }
-                }
-            }
-
-            return result;
-        }
-
-        public static T MapTo<T>(this object obj, params Converter[] converters) where T : new()
-        {
-            if (converters.Length == 0)
-                return MapTo<T>(obj, new DefaultConverterResolver());
+		public static T MapTo<T>(this object obj, params Converter[] converters) where T : new()
+		{
+			if (converters.Length == 0)
+				return MapTo<T>(obj, new DefaultConverterResolver());
             
-            ConverterResolver converterResolver = new StandartConverterResolver(true);
+			ConverterResolver converterResolver = new StandartConverterResolver(true);
 
-            foreach (var converter in converters)
-            {
-                converterResolver.Add(converter);
-            }
+			foreach (var converter in converters)
+			{
+				converterResolver.Add(converter);
+			}
 
-            return MapTo<T>(obj, converterResolver);
+			return MapTo<T>(obj, converterResolver);
 
-            #region old
+			#region old
 
-            //T result = new T();
+			//T result = new T();
 
-            //foreach (var propertyInfo in result.GetType().GetProperties())
-            //{
-            //    PropertyNameAttribute t = (PropertyNameAttribute)Attribute.GetCustomAttribute(propertyInfo, typeof(PropertyNameAttribute));
+			//foreach (var propertyInfo in result.GetType().GetProperties())
+			//{
+			//    PropertyNameAttribute t = (PropertyNameAttribute)Attribute.GetCustomAttribute(propertyInfo, typeof(PropertyNameAttribute));
 
-            //    if (t != null)
-            //    {
-            //        PropertyInfo sourceProperty;
+			//    if (t != null)
+			//    {
+			//        PropertyInfo sourceProperty;
 
-            //        if (t.Type == null)
-            //        {
-            //            sourceProperty = obj.GetType().GetProperty(t.Name);
-            //        }
-            //        else
-            //        {
-            //            sourceProperty = obj.GetType().GetProperty(t.Name, t.Type);
-            //        }
+			//        if (t.Type == null)
+			//        {
+			//            sourceProperty = obj.GetType().GetProperty(t.Name);
+			//        }
+			//        else
+			//        {
+			//            sourceProperty = obj.GetType().GetProperty(t.Name, t.Type);
+			//        }
 
-            //        if (sourceProperty == null)
-            //        {
-            //            throw new InvalidMapException("There is not a property on the DB object which matches with " + t.Name + " from the domain object");
-            //        }
+			//        if (sourceProperty == null)
+			//        {
+			//            throw new InvalidMapException("There is not a property on the DB object which matches with " + t.Name + " from the domain object");
+			//        }
 
-            //        try
-            //        {
-            //            propertyInfo.SetValue(result, sourceProperty.GetValue(obj, null), null);
-            //        }
-            //        catch (Exception e)
-            //        {
-            //            throw new InvalidMapException(string.Format("The types of the properties {0} on {1} and {2} on {3} do not match. " +
-            //                                                        "See Inner Exception for more details.", propertyInfo.Name,
-            //                result.GetType().Name, sourceProperty.Name, obj.GetType().Name), e);
-            //        }
-            //    }
-            //}
+			//        try
+			//        {
+			//            propertyInfo.SetValue(result, sourceProperty.GetValue(obj, null), null);
+			//        }
+			//        catch (Exception e)
+			//        {
+			//            throw new InvalidMapException(string.Format("The types of the properties {0} on {1} and {2} on {3} do not match. " +
+			//                                                        "See Inner Exception for more details.", propertyInfo.Name,
+			//                result.GetType().Name, sourceProperty.Name, obj.GetType().Name), e);
+			//        }
+			//    }
+			//}
 
-            //return result;
+			//return result;
 
-            #endregion
-        }
-    }
+			#endregion
+		}
+
+		public static T MapToWithoutAttribute<T>(this object obj, params Converter[] converters) where T : new()
+		{
+			if (converters.Length == 0)
+				return MapToWithoutAttribute<T>(obj, new DefaultConverterResolver());
+            
+			ConverterResolver converterResolver = new StandartConverterResolver(true);
+
+			foreach (var converter in converters)
+			{
+				converterResolver.Add(converter);
+			}
+
+			return MapToWithoutAttribute<T>(obj, converterResolver);
+		}
+
+		public static T MapToWithoutAttribute<T>(this object obj, ConverterResolver converterResolver) where T : new()
+		{
+			return Map<T>(obj, converterResolver, false);
+		}
+	}
 }
